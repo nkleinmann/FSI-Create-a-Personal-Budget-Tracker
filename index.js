1ded337f498a2384c8a31745c712b79db264c08c
nkleinmann <nekleinmann@gmail.com>
2022-01-23 14:24:57 -0500

    add solutions

--- index.js
+++ index.js
@@ -3,3 +3,22 @@ let monthlyExpenses = 0
 let annualExpenses = 0
 
 // Your Code Here
+// let stringAnswer = window.prompt(weeklyExpenseQuestions[0])
+// console.log(answer) note that answer changed to be stringAnswer
+// let numberAnswer = parseFloat(stringAnswer)
+// console.log(numberAnswer)
+
+for(let i = 0; i < weeklyExpenseQuestions.length; i++) {
+    let answer = parseFloat(window.prompt(weeklyExpenseQuestions[i]))
+    weeklyExpenses = weeklyExpenses + answer
+}
+
+// for(let i = 0; i < monthlyExpenseQuestions.length; i++) {
+//     let answer = parseFloat(window.prompt(monthlyExpenseQuestions[i]))
+//     monthlyExpenses = monthlyExpenses + answer
+// }
+
+// for(let i = 0; i < annualExpenseQuestions.length; i++) {
+//     let answer = parseFloat(window.prompt(annualExpenseQuestions[i]))
+//     annualExpenses = annualExpenses + answer
+// }
